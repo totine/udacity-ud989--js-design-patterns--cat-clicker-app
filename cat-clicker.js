4550852fcc202192f7d19ae2178aa77a23162122
Joanna <joanna.gargas@gmail.com>
2017-12-08 22:37:53 +0100

    Implement cat-clicker after changes of requirements

--- cat-clicker.js
+++ cat-clicker.js
@@ -1,9 +1,22 @@
 
-
-var elem = document.getElementById('cat-image');
+var numberOfCats = 2;
+var catsToClick = [];
 var counter = document.getElementById('click-counter');
-elem.addEventListener('click', function(){
-    var counterValue = counter.innerText;
-    counterValue++;
-    counter.innerText = counterValue;
-}, false);
+var catsNames = ['Nick', 'Mruczek'];
+
+
+for (var i=1; i<=numberOfCats; i++) {
+    catsToClick.push(document.getElementById('cat' + i + '-image'));
+    document.getElementById('cat' + i + '-name').innerText = catsNames[i-1];
+}
+
+
+for (i=0; i<catsToClick.length; i++) {
+    catsToClick[i].addEventListener('click', function() {
+            var counterValue = counter.innerText;
+            counterValue++;
+            counter.innerText = counterValue;
+        }
+            , false);
+}
+
