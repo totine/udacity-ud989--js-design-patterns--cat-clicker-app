f074356229b2938739621a8279ef84b120f25b2f
Joanna <joanna.gargas@gmail.com>
2017-12-08 22:12:03 +0100

    Implement the first simple cat clicker code

--- cat-clicker.js
+++ cat-clicker.js
@@ -1,3 +1,9 @@
-/**
- * Created by joanna on 28.11.17.
- */
+
+
+var elem = document.getElementById('cat-image');
+var counter = document.getElementById('click-counter');
+elem.addEventListener('click', function(){
+    var counterValue = counter.innerText;
+    counterValue++;
+    counter.innerText = counterValue;
+}, false);
